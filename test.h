9031b9ab386164c8f4c007c2ae41d3c5d1a43431
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2021-10-30 20:07:32 +0300

    Named all test

---
+++ test.h
@@ -0,0 +1,13 @@
+#ifndef TEST_H
+#define TEST_H
+
+
+#include <assert.h>
+#include "meteocenter.h"
+
+void weatherStationConstructorTest();
+void weatherStationSetGetTest();
+void meteoCenterConstructorTest();
+void weatherVaneConstructorTest();
+
+#endif // TEST_H
