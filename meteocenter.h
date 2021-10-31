2a551f4c4247671ed20b4b38156077a73fac8429
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2021-10-31 11:47:26 +0300

    Class collection

---
+++ meteocenter.h
@@ -0,0 +1,30 @@
+#ifndef METEOCENTER_H
+#define METEOCENTER_H
+
+#include "weatherstation.h"
+#include <string>
+
+class MeteoCenter {
+    public:
+        MeteoCenter(int newSideLentgh);
+        MeteoCenter(const MeteoCenter& other);
+
+        //WeatherVane getElement(int a, int b) const;
+        void deleteAll();
+
+        int getCountEl() const;
+        int getSideLength() const;
+
+        void importFromFile(const std::string path);
+        void exportToFile(std::string path) const;
+
+        [[nodiscard]] std::string* getWeatherInPoint(int row, int column) const;
+
+        ~MeteoCenter();
+
+    private:
+        WeatherVane** matrix;
+        int sideLength;
+};
+
+#endif // METEOCENTER_H
