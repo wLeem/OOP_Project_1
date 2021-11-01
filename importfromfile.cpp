b89cb185c986a403f54522bffd94c2c048092c18
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2021-11-01 17:49:39 +0300

    Get data from file

---
+++ importfromfile.cpp
@@ -0,0 +1,33 @@
+#include "importfromfile.h"
+#include "ui_importfromfile.h"
+#include <QString>
+#include <string>
+#include <QMessageBox>
+#include <QFileDialog>
+
+ImportFromFile::ImportFromFile(QWidget *parent): QDialog(parent), ui(new Ui::ImportFromFile) {
+    ui->setupUi(this);
+}
+
+ImportFromFile::~ImportFromFile() {
+    delete ui;
+}
+
+void ImportFromFile::on_pushButton_clicked() {
+    ps = new MeteoCenter(0);
+//    QString path = ui->lineEdit->text();
+//    std::string pathStr = path.toStdString();
+//    ps->importFromFile(pathStr);
+//    *mps = *ps;
+    QString str;
+    str = QFileDialog::getOpenFileName();
+    std::string pathStr = str.toStdString();
+    ps->importFromFile(pathStr);
+    *mps = *ps;
+
+    QMessageBox::information(this,"","Данные были импортированы");
+}
+
+void ImportFromFile::recData(MeteoCenter *ms){
+     mps = ms;
+}
