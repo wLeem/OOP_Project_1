c5f69ef95d95700da273e1f92e5752541c98ab54
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2021-11-03 09:32:04 +0300

    Child class

--- weatherstation.cpp
+++ weatherstation.cpp
@@ -4,18 +4,12 @@
 #include <iostream>
 
 WeatherStation::WeatherStation(){
-    //WeatherVane();
-    //WeatherVane(0, 0);
     temperature = 0;
     pressure = 0;
-    //windDirection = 0;
-    //windSpeed = 0;
 }
 
 WeatherStation::WeatherStation(int temp, int press, int windDir, int windSp) :
     WeatherVane(windDir, windSp), temperature(temp), pressure(press) {}
-    //windDirection = windDir;
-    //windSpeed = windSp;
 
 
 int WeatherStation::getTemperature() const {
@@ -32,27 +26,9 @@ void WeatherStation::setPressure(const int x) {
     pressure = x;
 }
 
-/*int WeatherStation::getWindDirection() const {
-    return windDirection;
-}
-void WeatherStation::setWindDirection(const int x) {
-    windDirection = x;
-}
-
-int WeatherStation::getWindSpeed() const {
-    return windSpeed;
-}
-void WeatherStation::setWindSpeed(const int x) {
-    windSpeed = x;
-}*/
-
 int WeatherStation::getType() const {
     return 2;
 }
-
-/*void WeatherStation::setType() {
-    typeW = 2;
-}*/
 
 void WeatherStation::operator=(const WeatherStation& WS) {
     this->temperature = WS.temperature;
